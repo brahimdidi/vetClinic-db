b57bb6a9e98dd278c84acd854dc636d0fae00240
Brahim didi <brahimgb1team@gmail.com>
2022-07-15 21:57:19 +0000

    fix line 5 use less than instead of more than.

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animals WHERE name like '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name FROM animals WHERE neutered = true AND escape_attempts > 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name,escape_attempts FROM animals WHERE  weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
