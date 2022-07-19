813b76663275e28dd96029998563382de54cf8ae
Brahim didi <brahimgb1team@gmail.com>
2022-07-19 18:14:05 +0000

    remove unwanted changes.

--- queries.sql
+++ queries.sql
@@ -8,10 +8,3 @@ SELECT name,escape_attempts FROM animals WHERE  weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
-SELECT COUNT(*) FROM animals;
-SELECT COUNT(*) FROM animals WHERE escape_attempts<1;
-SELECT AVG(weight_kg) from animals;
-SELECT SUM(escape_attempts) FROM animals GROUP BY neutered;
-SELECT MIN(weight_kg) FROM animals GROUP BY species;
-SELECT AVG(weight_kg) from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
