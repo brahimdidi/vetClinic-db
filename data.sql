f53f6b76993ec4b57d7b57f5238971a5e37ff717
Brahim didi <brahimgb1team@gmail.com>
2022-07-20 15:07:48 +0000

    insert data into the tables owners and species

--- data.sql
+++ data.sql
@@ -23,3 +23,20 @@ VALUES ('Angemon',DATE '2005-06-12',1,true,-45);
  VALUES ('Blossom',DATE '1998-10-13',3,true,17);
  INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ('Ditto',DATE '2022-05-14',4,true,22);
+
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+ 
+ 
